cf0f97846230d11f5fe8931df4754869edcb5169
Friedemann Baumgaertel <friedemann1801@gmail.com>
2023-03-22 12:33:19 +0100

    fixing

--- gymwebsite/src/index.js
+++ gymwebsite/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Components/App"
 import PlansPage from "./Components/PlansPage"
+import Checkout from "./Components/Checkout"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 class Website extends React.Component {
@@ -12,6 +13,7 @@ class Website extends React.Component {
                 <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/Plans" element={<PlansPage />} />
+                    <Route path="/Checkout" element={<Checkout />} />
                 </Routes>
             </BrowserRouter>
             )
